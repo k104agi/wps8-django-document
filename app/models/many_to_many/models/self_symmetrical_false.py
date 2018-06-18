b0f9081214edc4ed92cae9b60477cefb5234bdf7
kjs <k104agi@gmail.com>
2018-06-18 15:24:47 +0900

    InstagramUser 구현

---
+++ app/models/many_to_many/models/self_symmetrical_false.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+__all__ = (
+    'InstagramUser',
+)
+
+
+class InstagramUser(models.Model):
+    name = models.CharField(max_length=50)
+
+    # 내가 follow하고 있는 사람 목록 만들기
+    # 대칭적이지 않은 다대다 관계
+    follow = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name = 'followers',
+    )
+
+    def __str__(self):
+        return self.name
