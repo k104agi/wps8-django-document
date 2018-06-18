29e85305f4e8b3e770b0ef36d1552f0081f1ca1d
kjs <k104agi@gmail.com>
2018-06-18 15:07:54 +0900

    show_friends() 함수 구현

--- app/models/many_to_many/models/self.py
+++ app/models/many_to_many/models/self.py
@@ -3,6 +3,8 @@ from django.db import models
 __all__ = (
     'FacebookUser',
 )
+
+
 # 튜플은 1개라도 항상 맨 뒤가 ,로 끝나야 함
 
 
@@ -19,3 +21,31 @@ class FacebookUser(models.Model):
 
     def __str__(self):
         return self.name
+
+    def show_friends(self):
+        # 본인의 친구목록
+        # 이한영의 친구목록
+        # - 천이수
+        # - 박성민
+        # (총 2명)
+
+        friends_list = list()
+        print(self.friends)
+        print('위는 self.friends 출력 결과입니다')
+        print()
+
+        print(self.friends.all())
+        print('위는 self.friends.all() 출력 결과입니다')
+        print()
+
+        for x in self.friends.all():
+            friends_list.append(x)
+
+        # 리스트에 인덱스 연산 사용
+        print(f'- {friends_list[0]}\n'
+              f'- {friends_list[1]}\n'
+              f'(총 {len(friends_list)}명)')
+
+        # print(f'- {self.friends.first()} \n '
+        #       f'- {self.friends.second()}\n '
+        #       f'(총 {len(self.friends)}명)')
