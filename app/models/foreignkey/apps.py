9d375742597b6ddf947ebe50839d9a8372dea709
kjs <k104agi@gmail.com>
2018-06-18 11:43:15 +0900

    ForeignKey까지

--- app/models/foreignkey/apps.py
+++ app/models/foreignkey/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ForeignkeyConfig(AppConfig):
     name = 'models.foreignkey'
+    # 소스루트(시작경로)가 app이어서 ./models/foreignkey인 셈
--- app/models/foreignkey/models.py
+++ app/models/foreignkey/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
 
+class Car(models.Model):
+    # 제조사가 먼저 있어야 차를 생성할 수 있음
+    manufacturer = models.ForeignKey(
+        'Manufacturer',
+        # 원래 그냥 Manufacturer이고
+        # Manufacturer 클래스 먼저 정의하고 아래에 Car가 와야함
+        # 하지만 ''를 붙여주면 해당 정의가 아래에 나와도 알아서 찾음(장고기능)
+        on_delete=models.CASCADE,
+        # DB 상에서 제조사가 사라지면 같이 사라짐
+    )
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Manufacturer(models.Model):
     name = models.CharField(max_length=50)
 
@@ -8,12 +24,16 @@ class Manufacturer(models.Model):
         return self.name
 
 
-class Car(models.Model):
-    manufacturer = models.ForeignKey(
-        Manufacturer,
-        on_delete=models.CASCADE,
-    )
+class User(models.Model):
     name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
+    instructor = models.ForeignKey(
+        # foreignkey이지만 self(자기자신)를 참조함
+        'self',
+        on_delete=models.SET_NULL,
+        # DB상에서 강사가 사라진다해도 자기를 삭제하는게 아니라 NULL로 변경함
+        blank=True,
+        null=True,
+        # null을 허용
+        related_name='students',
+        # 관계명을 새로 지정해줌(원래는 소문자이름_set임)
+    )
